d957f24da9b1fadc79cdf6a940c109b26f2410c2
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-10-20 22:10:18 +0530

    Folder Structure.

---
+++ Modern JS (ES6)/Stack_01.js
@@ -0,0 +1,25 @@
+// let = blocked scoped variable
+//const - constant varibale (no further changes allowed.)
+
+//Template Literals : `${variable}`
+
+//Default Parameters
+
+// Default function parameters allow named parameters to be
+// initialized with default values if no value or undefined is passed.
+
+function mult(a, b = 5) {
+  return a * b;
+} // (b)here is a default parameter.
+
+console.log(mult(3));
+
+//Fat-Arrow Functions in Javascript
+
+const result = (maths, physics) => {
+  return (total = maths + physics);
+};
+
+console.log(result(70, 70));
+
+//Return keyword can be avoided if the function is a one-line function.
