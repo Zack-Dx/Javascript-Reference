bd399951f7ae5eb1d5735404df02dbe41cd4dbc4
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-10-21 16:07:19 +0530

    Javascript Strings

---
+++ Modern JS (ES6)/Strings/Methods.js
@@ -0,0 +1,25 @@
+// 👉 Finding a String in a String
+
+// String.prototype.indexOf(searchValue [, fromIndex]) 🙋‍♂️
+
+// The indexOf() method returns the index of (the position of) the first
+//  occurrence of a specified text in a string
+
+// const myBioData =  'I am Zack Dx';
+// console.log(myBioData.indexOf("c", 1));
+
+
+// // JavaScript counts position from zero.
+// // 0 is the first position in a string, 1 is the second, 2 is the third ...
+
+
+
+
+// // String.prototype.lastIndexOf(searchValue [, fromIndex]) 🙋‍♂️
+// // Returns the index within the calling String object of the 
+// // last occurrence of searchValue, or -1 if not found.
+
+
+// const myBioData =  'I am Zack Dx';
+// console.log(myBioData.lastIndexOf("Z", 8));
+
