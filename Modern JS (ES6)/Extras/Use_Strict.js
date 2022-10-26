84315aa657cc9264a0adea86693dd3d1b5106a65
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-10-26 23:25:18 +0530

    Use Strict and This Keyword.

---
+++ Modern JS (ES6)/Extras/Use_Strict.js
@@ -0,0 +1,6 @@
+// Use Strict Mode in Javascript:
+
+
+// This Mode instructs the browsers to use the strict mode, which is reduced and safer side of Javascript.
+
+// Always use Strict Mode.
---
+++ Modern JS (ES6)/Extras/this_keyword.js
@@ -0,0 +1,27 @@
+// `this` keyword refers to the object it belongs to in javascript.
+
+// It has different values because it's based where it's usec.
+
+// Alone, this refers to the Global Object. basically (Window Object)
+
+// Case :1 (this inside a regular function.)
+
+function check() {
+  console.log(this);
+}
+
+check(); // This will represent to the Window Object.
+
+// Case:2 (this inside a method)
+
+const user = {
+  name: "Zack",
+  qualification: "JS developer",
+
+  checker() {
+    console.log(this.name); // Here this will refer to the Owner Object as `this` is logged inside a Method.
+  },
+};
+
+user.checker();
+
