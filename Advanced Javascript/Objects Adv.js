2c9dcb75b72a20cd3afd675afb76fbac607a38db
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-10-27 21:23:55 +0530

    Objects

---
+++ Advanced Javascript/Objects Adv.js
@@ -0,0 +1,3 @@
+// Object.keys(Object) Returns an array of the object's key names.
+// Object.values(Object) Returns an array of the object's values.
+// Object.entries(Object) Returns an array of KEY:VALUE pair.
