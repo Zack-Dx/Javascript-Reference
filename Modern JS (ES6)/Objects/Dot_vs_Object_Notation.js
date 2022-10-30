aa849029726023ed22f62d840d8816db2b499358
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-10-30 09:23:17 +0530

    Dot notation vs Accessing like array.

---
+++ Modern JS (ES6)/Objects/Dot_vs_Object_Notation.js
@@ -0,0 +1,18 @@
+let obj1 = {
+  name: "Zack",
+  work: "Great Check",
+};
+
+//Accessing Style (Array like )
+
+console.log(obj1["name"]);
+console.log(obj1["work"]);
+
+obj1["curious"] = "fox"; // Adding a property.
+console.log(obj1);
+
+//Playing with Variables.
+
+let a = "name";
+
+console.log(obj1[a]);
