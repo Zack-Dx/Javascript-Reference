1fdfb419cf78a6f6b89d89b694b3e92e6881e4d6
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-11-02 19:17:49 +0530

    IIFE

---
+++ Advanced Javascript/IIFE.js
@@ -0,0 +1,11 @@
+//IIFE is a javascript function which is called as soon as it is defined.
+
+// We need this in kind of a page load function call or priavte funcs.
+
+// Using Grouping Operator making a private func:
+
+(function () {
+  console.log(`Hello I'm an IFFE function.`);
+})();
+
+// We can't access anything inside IIFE function from outside.
